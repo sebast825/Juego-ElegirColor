c681477ddcd5de8e915b80bafdb1186f3b63b93b
sebast825 <sebamolina825@gmail.com>
2022-02-08 11:13:48 -0300

    cambia de color

--- modules/app.js
+++ modules/app.js
@@ -1,13 +1,19 @@
 
-const menu = document.querySelector('.menu')
+const menu = document.querySelector('.menu');
+const btn = document.querySelectorAll('.btn');
 
+import { Quiz } from "./Quiz.js";
 import { Tablero } from "./Tablero.js"
 
+
+
 function main(){
-	const tablero = new Tablero()
-	tablero.createTablero()
-	tablero.numArray()
-	tablero.setColor()
+	const tablero = new Tablero();
+	tablero.createTablero();
+	tablero.numArray();
+	tablero.setColor();
+	// const qu = new Quiz();
+	
 }
 
 main()
--- modules/tablero.js
+++ modules/tablero.js
@@ -9,11 +9,18 @@ export class Tablero{
 			const btn = document.createElement('button');
 			btn.classList.add('btn');
 			btn.classList.add(`btn-${i}`)
+			btn.addEventListener('click',()=> {
+				
+				this.numArray()
+				this.setColor()
+			})
 			tablero.appendChild (btn);
 		}
 	}
 	//genera un numero apra que despues cada boton tenga un color
 	numArray(){
+		this.array = [];
+		
 		while(this.array.length < 4){
 			let mat = Math.floor(Math.random(1)*4)
 			if(!this.array.includes(mat)){
