56d2e75f83cce175d3c68b0240305d84735442fc
sebast825 <sebamolina825@gmail.com>
2022-05-28 19:25:08 -0300

    header

--- animations/menu.js
+++ animations/menu.js
@@ -43,26 +43,24 @@ export function eventMenu(){
 
 	menuBtn.forEach(element => {
 		element.addEventListener("mouseout",()=>{
-			let value = element.value;
-			customText.innerHTML = "Click yellow button";
+	
+			customText.innerHTML = "Click yellow button to win points!";
 
 	})
 	}); 
 }
 
 function derivarAnimacion(value){
-	let text = document.createElement("h2");
+
 	if(value=="Clasico"){
-		customText.innerHTML = "Do 11 points to Win!";
-	
+		customText.innerHTML = "Do 11 points to Win!";	
 	}else if(value=="ContraReloj"){
 		customText.innerHTML = "You have 40 seconds to do all the points you can!";
 	}else if(value=="DeathMode"){
 		customText.innerHTML = "Win Points to win time, Be Carefull! If you miss, will lose!";
 	}else{
-		customText.innerHTML = "Click yellow button";
+		console.log('error');
 		}
-
 
 
 }
